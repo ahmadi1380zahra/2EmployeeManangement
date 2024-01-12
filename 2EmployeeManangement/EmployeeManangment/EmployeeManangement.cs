7ac6ae770c245fcfd4dfb8077406aa511ddaf287
ahmadi1380zahra <zahraahmaadi1380@gmail.com>
2024-01-12 13:28:05 +0330

    push stable version of project

---
+++ 2EmployeeManangement/EmployeeManangment/EmployeeManangement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManangment
+{
+    static class EmployeeManangement
+    {
+        private static List<Employee> employees = new();
+        private static int _id=0;
+        public static void AddEmployee(int type,string name,int salary,string departmentName)
+        {
+            if (type == 1)
+            {
+                var employee = new Employee(name,salary, ++_id);
+                employees.Add(employee);
+                employee.PrintDetail(employee.Name,employee.ID,employee.Salary);
+             }
+            if (type == 2)
+            {
+                var manager = new Manager(name, salary,++ _id, departmentName);
+                employees.Add(manager);
+                manager.PrintDetail(manager.Name, manager.ID, manager.Salary);
+            }
+        }
+        
+    }
+}
---
+++ 2EmployeeManangement/EmployeeManangment/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace EmployeeManangment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Run();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+            static void Run()
+            {
+                var option = GetInt("1.add employee");
+                switch (option)
+                {
+                    case 1:
+                        {
+                            string department=null;
+                            var name = GetString("Enter Employe Name");
+                            var salary = GetInt("Enter Employe salary");
+                            var employeType = GetInt("1.simple employee 2.manager");
+                            if (employeType != 2 & employeType != 1)
+                            {
+                                throw new Exception("No valid Type");
+                            }
+                            if (employeType == 2)
+                            {
+                                 department = GetString("Enter department Name");
+                            }
+                           
+                            EmployeeManangement.AddEmployee(employeType,name,salary,department);
+                            break;
+                        }
+
+                }
+            }
+
+        }
+        static string GetString(string message)
+        {
+            Console.WriteLine(message);
+            string value = Console.ReadLine()!;
+            return value;
+        }
+
+        static int GetInt(string message)
+        {
+            Console.WriteLine(message);
+            int value = int.Parse(Console.ReadLine()!);
+            return value;
+        }
+    }
+}
